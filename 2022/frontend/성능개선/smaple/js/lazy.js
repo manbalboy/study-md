8a008639e93517875d7cebb4b8fc32315e53e60e
manbalboy <manbalboy@hanmail.net>
2022-12-11 16:21:08 +0900

    [2022][성능개선]script 위치/브라우저 랜더링 과정 및 성능지표 정리

---
+++ 2022/frontend/성능개선/smaple/js/lazy.js
@@ -0,0 +1,10 @@
+const lazy = () => {
+
+  // document.body.style.display = "none";
+  for(let i = 0 ; i< 100000; i++) {
+    console.log(i);
+  }
+  // document.body.style.display = "";
+}
+
+lazy()
